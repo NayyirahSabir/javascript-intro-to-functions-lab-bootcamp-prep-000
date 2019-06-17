fcfb84fb2ded09788664dec57e768f8b058a90d0
nsabir <awesomeartist15@gmail.com>
2019-06-17 01:42:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string){
+  return "I can't hear you!".toUpperCase()
+  return "I love you,too.".toLowerCase()
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
